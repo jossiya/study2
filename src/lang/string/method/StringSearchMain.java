addfa7a674a379f6f2d29e4fbc615f2810142f0c
AIM <rolety2202@naver.com>
2024-07-29 14:53:18 +0900

    String에 대한 여러가지 메스드

---
+++ src/lang/string/method/StringSearchMain.java
@@ -0,0 +1,10 @@
+package lang.string.method;
+
+public class StringSearchMain {
+    public static void main(String[] args) {
+        String str = "Hello, Java! Welcome to Java world";
+        System.out.println("문자열에 'Java'가 포함 되어 있는지: " + str.contains("Java!"));
+        System.out.println("'Java'의 첫 번재 인덱스: " + str.indexOf("Java"));
+        System.out.println("인덱스 10부터 'Java'의 인덱스 : " + str.indexOf("Java", 10) );
+    }
+}
